42330bd3ef36eca56e850b05e5efb33197c0e68d
John <johnlawless.music@gmail.com>
2019-07-17 14:21:35 -0400

    Node script added

---
+++ Node.py
@@ -0,0 +1,13 @@
+class Node:
+  def __init__(self, value, linked_node = None):
+    self.value = value
+    self.linked_node = linked_node
+  
+  def get_value(self):
+    return self.value
+  
+  def get_linked_node(self):
+    return self.linked_node
+
+  def set_linked_node(self, new_linked_node):
+    self.linked_node = new_linked_node
